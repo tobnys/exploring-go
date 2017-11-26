8a519e52b864ccb8694dc34a4129ce6698e4a5b0
tobnys <tobnystrom@gmail.com>
2017-11-26 01:01:56 +0100

    variables, types

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	var hello int = 123123
+	var floatVar float32 = 123.4
+
+	floatVar2 := 123.61
+	stringVar := "Hello"
+
+	fmt.Println(hello, floatVar, floatVar2, stringVar)
 }
