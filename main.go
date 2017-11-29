4f1da73e68960f8d7e2bcc4b6312404705d90c95
tobnys <tobnystrom@gmail.com>
2017-11-29 16:28:00 +0100

    Testing the fmt package

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test")
-	fmt.Printf(w)
+	fmt.Fprintf(w, "Test") // Response
+	// %T = TYPEOF
+	//fmt.Printf("%T", w) // *http.response*http.responsee
+	// %v = VALUE
+	fmt.Printf("%v", w) /* &{0xc04204c820 0xc0420f8000 {} 0x50d490 true false false false 0xc04203e2c0 {0xc042108000 map[] false false} map[] false 4 -1 200 false false [] 0 [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0
+	0 0 0 0] [0 0 0] 0xc042040070 0}&{0xc04204c820 0xc042118000 {} 0x50d490 true false false false 0xc04210e080 {0xc04212c000 map[] false false} map[] false 4 -1 200 false false [] 0 [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+	0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0 0] [0 0 0] 0xc04212a000 0}
+	*/
+
+	fmt.Printf("%v", r)
 }
