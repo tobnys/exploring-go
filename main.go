9bb147062cdabcdab4c93644345e7815d009d0e1
tobnys <tobnystrom@gmail.com>
2017-12-16 23:09:32 +0100

    Seeding time

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano()) // https://golang.org/pkg/time/#Time.UnixNano
+}
 
 func main() {
 	var c int64 = 51231241234123123
