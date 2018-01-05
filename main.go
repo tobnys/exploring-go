6561c274546f288e9efc021e63db360062979fc5
tobnys <tobnystrom@gmail.com>
2018-01-05 23:38:21 +0100

    Dynamically-sized arrays & insertion

--- main.go
+++ main.go
@@ -15,4 +15,16 @@ func main() {
 
 	d := a[1:3]
 	fmt.Println(d)
+
+	// Dynamically sized arrays
+	length := 5
+	capacity := 5
+	x := make([]int, length, capacity)
+	fmt.Println(x)
+
+	// Appending to a slice
+	// https://golang.org/pkg/builtin/#append
+	x = append(x, 1, 2, 3)
+	fmt.Println(x)
+
 }
